8f0213a43ea9606668a0c6fc1d48fb2099873ed0
Sokos6 <will.sokolowski@gmail.com>
2021-02-16 21:52:47 -0600

    app init

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useWeb3 } from '@openzeppelin/network/react';
+
+const infuraProjectId = '3c3e47b42c5749f7abd077faaa924e7f';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const web3Context = useWeb3(
+        `wss://mainnet.infura.io/ws/v3/${infuraProjectId}`
+    );
+    const { networkId, networkName, providerName } = web3Context;
+
+    return (
+        <div className="App">
+            <div>
+                <h1>PUNKER</h1>
+                <div>
+                    Network:{' '}
+                    {networkId
+                        ? `${networkId} - ${networkName}`
+                        : 'No connection'}
+                </div>
+                <div>Provider: {providerName}</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
