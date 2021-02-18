04419ba679d9c09609b24136091bcab47387c782
Sokos6 <will.sokolowski@gmail.com>
2021-02-17 23:41:39 -0600

    functional frontent with metamask connect

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
-import './App.css';
-import Web3Data from './components/Web3Data';
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 import { useWeb3 } from '@openzeppelin/network/react';
+
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = (page) => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
 
 const infuraProjectId = '3c3e47b42c5749f7abd077faaa924e7f';
 
-function App() {
-    const web3Context = useWeb3(
-        `wss://mainnet.infura.io/ws/v3/${infuraProjectId}`
-    );
-    return (
-        <div className="App">
-            <div>
-                <h1>Cryptofrequency</h1>
-                <Web3Data title="Web3 Data" web3Context={web3Context} />
-            </div>
-        </div>
-    );
-}
+const App = () => {
+  const childRef = useRef();
+  let location = useLocation();
+
+  const web3Context = useWeb3(
+    `wss://rinkeby.infura.io/ws/v3/${infuraProjectId}`
+  );
+  const { networkId, networkName, providerName } = web3Context;
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded');
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  return (
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )}
+    />
+  );
+};
 
 export default App;
