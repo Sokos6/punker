fd8eae5fbe7bff2717192d6c9923457aacb0fdf0
Sokos6 <will.sokolowski@gmail.com>
2021-02-17 22:32:58 -0600

    working metamask integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Web3Data from './components/Web3Data';
 import { useWeb3 } from '@openzeppelin/network/react';
 
 const infuraProjectId = '3c3e47b42c5749f7abd077faaa924e7f';
@@ -7,19 +8,11 @@ function App() {
     const web3Context = useWeb3(
         `wss://mainnet.infura.io/ws/v3/${infuraProjectId}`
     );
-    const { networkId, networkName, providerName } = web3Context;
-
     return (
         <div className="App">
             <div>
-                <h1>PUNKER</h1>
-                <div>
-                    Network:{' '}
-                    {networkId
-                        ? `${networkId} - ${networkName}`
-                        : 'No connection'}
-                </div>
-                <div>Provider: {providerName}</div>
+                <h1>Cryptofrequency</h1>
+                <Web3Data title="Web3 Data" web3Context={web3Context} />
             </div>
         </div>
     );
